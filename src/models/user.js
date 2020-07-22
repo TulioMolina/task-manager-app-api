a059dda3cfe2ba90b8ac6d255e0c6ad55e252898
Tulio Molina <tuliojose8@gmail.com>
2020-07-22 18:12:13 -0400

    cascade delete on tasks implemented

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const Task = require("./task");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -92,6 +93,21 @@ userSchema.pre("save", async function (next) {
   }
 
   next();
+});
+
+// middleware to delete user tasks when user is removed
+userSchema.pre("remove", async function (next) {
+  const user = this;
+
+  await Task.deleteMany({ owner: user._id });
+
+  next();
+});
+
+userSchema.virtual("tasks", {
+  ref: "Task",
+  localField: "_id",
+  foreignField: "owner",
 });
 
 const User = mongoose.model("User", userSchema);
