8349d6bd286c5eefbcbeec4a2c27e341c12f4238
Tulio Molina <tuliojose8@gmail.com>
2020-09-12 14:09:12 -0400

    handler for get request on root path added

--- src/app.js
+++ src/app.js
@@ -8,4 +8,11 @@ app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
 
+app.get("/", (req, res) => {
+  res.send({
+    message:
+      "Welcome to the task management REST API, for usage details and availbale endpoints please refer to the API reference on https://github.com/TulioMolina/task-management-api#api-reference",
+  });
+});
+
 module.exports = app;
